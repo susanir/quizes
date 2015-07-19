60819a08105b19a694c39feeb06e3d621c276aa6
Susana Iñiguez <susan_susan_susan1234@yahoo.es>
2015-07-19 19:57:07 +0200

    Despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controller/quiz_controller');
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
